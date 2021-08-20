023ad7a2a4eabad772e4225b2ad5514852ecb53f
mateovich <magavich@protonmail.com>
2021-08-20 14:07:24 -0300

    segundo commit

--- js/login.js
+++ js/login.js
@@ -2,5 +2,37 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("entrar").addEventListener("click", function () {
+        let usuario = document.getElementById("user");
+        let contrasenia = document.getElementById("contrasenia");
+        let todolisto = false;
+        let usuarioIngresado = false;
+        let contraseniaIngresada = false;
 
+        if (user.value === "") {
+            usuario.classList.add("vacio");
+            usuarioIngresado = false;
+        } else {
+                usuarioIngresado = true;
+                usuario.classList.remove("vacio");
+            }
+        if (contrasenia.value === "") {
+            contrasenia.classList.add("vacio");
+            contraseniaIngresada = false;
+        } else {
+            contraseniaIngresada = true;
+            contrasenia.classList.remove("vacio");
+        }
+        if (contraseniaIngresada && usuarioIngresado) {
+            todolisto = true;
+        } else {
+            todolisto = false;
+        }
+        if (todolisto) {
+            alert("bienvenido");
+            window.location = "inicio.html";
+        } else {
+            alert("ingrese todo los datos");
+        }
+    });
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,28 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var lista_productos = [];
 
+function listar(productos) {
+    let cont_productos = "<br><hr><br>";
+    for (let i = 0; i< productos.length; i++) {
+        let producto = productos[i];
+
+        cont_productos += producto.name + "<br>";
+        cont_productos += producto.description + "<br>";
+        cont_productos += "$$ " + producto.cost + " " + producto.currency + "      ";
+        cont_productos += "vendidos: " + producto.soldCount + "<br><hr><br>";
+
+    }
+document.getElementById("bloqueProductos").innerHTML = cont_productos;
+};
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(result){
+        if (result.status === "ok") {
+            lista_productos = result.data;
+            listar(lista_productos);
+        }
+    })
 });
+
